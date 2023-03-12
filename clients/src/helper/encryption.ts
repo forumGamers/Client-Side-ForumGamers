a18423704668fb20258e42ad74078652811d7afb
anandafiqri <anandafiqri1403@gmail.com>
2023-03-12 16:26:28 +0700

    setup

---
+++ clients/src/helper/encryption.ts
@@ -0,0 +1,13 @@
+import { AES, enc } from "crypto-ts";
+require("dotenv").config();
+const key: string = process.env.ENCRYPTION_KEY as string;
+
+export default abstract class Encryption {
+  public static encrypt(data: string): string {
+    return AES.encrypt(data, key).toString();
+  }
+
+  public static decrypt(data: string): string {
+    return AES.decrypt(data, key).toString(enc.Utf8);
+  }
+}
---
+++ clients/src/pages/login/index.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+  return (
+    <div>
+      <h1>Ok</h1>
+    </div>
+  );
+}
