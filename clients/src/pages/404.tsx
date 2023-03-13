b93b33a87fd8770bfc7458229b43b5af44333cab
anandafiqri <anandafiqri1403@gmail.com>
2023-03-13 07:57:14 +0700

    fixing image

--- clients/src/pages/404.tsx
+++ clients/src/pages/404.tsx
@@ -1,17 +1,18 @@
 import Link from "next/link";
 import "@/styles/404.css";
-const image = "../../public/404.png";
+const image =
+  "https://ik.imagekit.io/b8ugipzgo/FrontEnd/404.png?updatedAt=1678668766605";
 
-export default function Custom404() {
+export default function Custom404(): JSX.Element {
   return (
-    <div className="container body">
+    <div className="container">
       <div className="content">
         <div className="title">Oops!</div>
         <div className="subtitle">
           The page you're looking for doesn't exist.
         </div>
         <Link href="/">
-          <h6 className="btn">Go back home</h6>
+          <p className="btn">Go back home</p>
         </Link>
       </div>
       <div className="image-container">
