48034a32ad90f5f4911807dcfa0997cd43f127f3
anandafiqri <anandafiqri1403@gmail.com>
2023-05-02 12:44:44 +0700

    add _document

--- clients/src/pages/_app.tsx
+++ clients/src/pages/_app.tsx
@@ -15,21 +15,6 @@ export default function MyApp({
     <>
       <Head>
         <title>Forum Gamers</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Silkscreen&display=swap"
-          rel="stylesheet"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-          integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
-          crossOrigin="anonymous"
-          referrerPolicy="no-referrer"
-        />
       </Head>
       <ApolloProvider client={client}>
         <SessionProvider session={pageProps.session}>
