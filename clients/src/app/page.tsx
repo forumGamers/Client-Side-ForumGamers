d678d992ff41ce34e987b436c7b3c457d08c68b8
anandafiqri <anandafiqri1403@gmail.com>
2023-07-26 08:54:10 +0700

    add default tsx

--- clients/src/app/page.tsx
+++ clients/src/app/page.tsx
@@ -1,17 +1,5 @@
 import styles from "./page.module.css";
 import dinamic from "next/dynamic";
-const StoreSection = dinamic(
-  () => import("@/components/carousel/storeCarousel"),
-  {
-    loading: () => <Loading />,
-  }
-);
-const TourSection = dinamic(
-  () => import("@/components/carousel/tourCarousel"),
-  {
-    loading: () => <Loading />,
-  }
-);
 const PostSection = dinamic(() => import("@/components/views/post"), {
   loading: () => <Loading />,
 });
@@ -172,128 +160,6 @@ async function getPostData(): Promise<timeLine[]> {
   }
 }
 
-async function getStoreData(): Promise<storeData[]> {
-  try {
-    // const { data } = await client.query<{ getAllStore: storeData[] }>({
-    //   query: GETSTOREDATAFORSTOREPAGE,
-    //   variables: {
-    //     query: {
-    //       limit: Encryption.encrypt("6"),
-    //     },
-    //   },
-    // });
-
-    // return data.getAllStore;
-    return [
-      {
-        CreatedAt: "2023-03-30T14:08:57.710477+07:00",
-        ID: 1,
-        StoreStatus: {
-          id: 1, //ini kurang
-          name: "Basic",
-        },
-        active: true,
-        avg_rating: 5,
-        image: "",
-        name: "testing store",
-        rating_count: 2,
-        description: "", //ini kurang
-        followers: 1, //ini
-        Items: [], //ini
-        background: "", //ini
-        exp: 0, //ini
-        status_id: 1, //ini
-      },
-      {
-        CreatedAt: "2023-03-30T14:08:57.710477+07:00",
-        ID: 2,
-        StoreStatus: {
-          id: 1,
-          name: "Basic",
-        },
-        active: true,
-        avg_rating: 5,
-        image: "",
-        name: "apa aja",
-        rating_count: 2,
-        description: "", //ini kurang
-        followers: 1, //ini
-        Items: [], //ini
-        background: "", //ini
-        exp: 0, //ini
-        status_id: 1, //ini
-      },
-      {
-        CreatedAt: "2023-03-30T14:08:57.710477+07:00",
-        ID: 3,
-        StoreStatus: {
-          name: "Basic",
-          id: 1,
-        },
-        active: true,
-        avg_rating: 5,
-        image: "",
-        name: "tes123 store",
-        rating_count: 2,
-        description: "", //ini kurang
-        followers: 1, //ini
-        Items: [], //ini
-        background: "", //ini
-        exp: 0, //ini
-        status_id: 1, //ini
-      },
-    ];
-  } catch (err) {
-    return [];
-  }
-}
-
-async function getTourGameData(): Promise<game[]> {
-  try {
-    // const { data } = await client.query<{ getGameList: game[] }>({
-    //   query: GETGAMELIST,
-    // });
-
-    // return data.getGameList;
-    return [
-      {
-        description: "ngetes",
-        _id: "642664603bc312e11423a98f",
-        image:
-          "https://ik.imagekit.io/b8ugipzgo/gameImage/Flappy_Bird_icon_tOdeVaMZ5v.png",
-        name: "flappy bird",
-        type: "Single Player",
-      },
-      {
-        description: "ngetes",
-        _id: "642675aefddc53d5e5cfe97b",
-        image:
-          "https://ik.imagekit.io/b8ugipzgo/gameImage/angry_birds_qHour1mAY.jpg",
-        name: "angry bird",
-        type: "Single Player",
-      },
-      {
-        description: "ngetes",
-        _id: "6426764ae878a6a7e07da2cf",
-        image:
-          "https://ik.imagekit.io/b8ugipzgo/gameImage/angry_birds_2_6O1VxF2d4.jpg",
-        name: "angry bird 2",
-        type: "Single Player",
-      },
-      {
-        description: "ngetes",
-        _id: "642676d429cb7697238dde68",
-        image:
-          "https://ik.imagekit.io/b8ugipzgo/gameImage/angry_birds_rio_DP-YEesvP.jpg",
-        name: "angry bird rio",
-        type: "Single Player",
-      },
-    ];
-  } catch (err) {
-    return [];
-  }
-}
-
 export default async function Home(): Promise<JSX.Element> {
   let userSession: CustomSession | null = null;
   await checkServerSession((session) => {
@@ -301,14 +167,10 @@ export default async function Home(): Promise<JSX.Element> {
   });
 
   const postData = await getPostData();
-  const storeData = await getStoreData();
-  const gameData = await getTourGameData();
   return (
     <>
       <main className={styles.main}>
         <PostSection session={userSession} data={postData} />
-        <StoreSection data={storeData} />
-        <TourSection data={gameData} />
       </main>
     </>
   );
