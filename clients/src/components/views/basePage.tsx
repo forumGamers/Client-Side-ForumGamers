66c2804e1322b68d716c1c15c563a4c296e79c1e
anandafiqri <anandafiqri1403@gmail.com>
2023-08-13 17:07:20 +0700

    add footer

--- clients/src/components/views/basePage.tsx
+++ clients/src/components/views/basePage.tsx
@@ -1,6 +1,8 @@
 import { CustomSession } from "@/interfaces/global";
 import NavigationBar from "../navigationBar";
 import { checkServerSession } from "@/helper/global";
+import { Typography } from "@/components/material-tailwind";
+import Link from "next/link";
 
 export default async function BasePage({
   children,
@@ -17,7 +19,60 @@ export default async function BasePage({
         <NavigationBar session={userSession} />
         {children}
       </main>
-      {/* {footer} */}
+      <footer className="w-full bg-white p-8 bottom-0">
+        <div className="flex flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 bg-white text-center md:justify-between">
+          <img src="/img/logo-ct-dark.png" alt="logo-ct" className="w-10" />
+          <ul className="flex flex-wrap items-center gap-y-2 gap-x-8">
+            <li>
+              <Link href="/company/about">
+                <Typography
+                  as="a"
+                  color="blue-gray"
+                  className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500"
+                >
+                  About Us
+                </Typography>
+              </Link>
+            </li>
+            <li>
+              <Link href="/company/license">
+                <Typography
+                  as="a"
+                  color="blue-gray"
+                  className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500"
+                >
+                  License
+                </Typography>
+              </Link>
+            </li>
+            <li>
+              <Link href="/company/team">
+                <Typography
+                  as="a"
+                  color="blue-gray"
+                  className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500"
+                >
+                  Our Team
+                </Typography>
+              </Link>
+            </li>
+            <li>
+              <Typography
+                as="a"
+                href="mailto:forumgamersindo@gmail.com"
+                color="blue-gray"
+                className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500"
+              >
+                Contact Us
+              </Typography>
+            </li>
+          </ul>
+        </div>
+        <hr className="my-8 border-blue-gray-50" />
+        <Typography color="blue-gray" className="text-center font-normal">
+          &copy; 2023 Forum Gamers
+        </Typography>
+      </footer>
     </>
   );
 }
